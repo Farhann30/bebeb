6a19a9167252453d94195c47d714bd897c886d47
Nurfadillah Umar <itsennuu@gmail.com>
2024-11-13 17:38:46 +0800

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,60 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          normal: "#1570EF",
+          "normal-hover": "#0D57BE",
+          gray: "##727272",
+        },
+        secondary: {
+          black: "#151515",
+          white: "#FFFFFF",
+        },
+      },
+
+      fontSize: {
+        "body-xs": [
+          "12px",
+          {
+            lineHeight: "16px",
+          },
+        ],
+        "body-sm": [
+          "14px",
+          {
+            lineHeight: "20px",
+          },
+        ],
+        "body-base": [
+          "16px",
+          {
+            lineHeight: "24px",
+          },
+        ],
+        "title-sm": [
+          "20px",
+          {
+            lineHeight: "28px",
+          },
+        ],
+        "title-md": [
+          "28px",
+          {
+            lineHeight: "36px",
+          },
+        ],
+        "title-lg": [
+          "36px",
+          {
+            lineHeight: "44px",
+          },
+        ],
+        "display-xl": [
+          "64px",
+          {
+            lineHeight: "72px",
+          },
+        ],
       },
     },
   },
